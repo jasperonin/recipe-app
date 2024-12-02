0015fd6347e45b54a8a69908e055ac135adc8808
Leo <snzleo@gmail.com>
2024-12-02 08:30:24 +0800

    added pop up to delete recipe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
+import { AlertDialogDemo } from "./delete_button";
 import {
   Dialog,
   DialogTrigger,
@@ -248,7 +249,8 @@ export default function Home() {
               <p>{recipe.description}</p>
             </CardContent>
             <CardFooter className="flex justify-between">
-            <Button variant={"destructive"} onClick={()=>handleDelete(recipe.id)}>Delete Recipe</Button>
+            {/* <Button variant={"destructive"} onClick={()=>handleDelete(recipe.id)}>Delete Recipe</Button> */}
+            <AlertDialogDemo onDelete={handleDelete} id={recipe.id}/>
               {recipe.vegan && <Badge variant="default">Vegan!</Badge>}
             </CardFooter>
           </Card>
