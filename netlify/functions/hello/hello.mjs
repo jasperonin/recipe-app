9704e3fbabdf7fecaed0a8d371bb4355060b9b35
Leo <snzleo@gmail.com>
2025-06-11 15:06:02 +0800

    netlify functions

--- netlify/functions/hello/hello.mjs
+++ netlify/functions/hello/hello.mjs
@@ -1,13 +1,10 @@
-// Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
-export default (request, context) => {
-  try {
-    const url = new URL(request.url)
-    const subject = url.searchParams.get('name') || 'World'
-
-    return new Response(`Hello ${subject}`)
-  } catch (error) {
-    return new Response(error.toString(), {
-      status: 500,
-    })
-  }
+export async function handler(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache"
+    },
+    body: JSON.stringify({ message: "Hello from Netlify Function!" }),
+  };
 }
